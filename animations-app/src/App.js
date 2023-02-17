dbbabce4e01c38f231a561e9aef480f1b7c12f8b
Shandira <shandira.ferguson@gmail.com>
2023-02-16 21:51:40 -0800

    BarChart animation succefully integrated

--- animations-app/src/App.js
+++ animations-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import BarChart from './Chart/BarChart';
 
 function App() {
 
@@ -10,21 +11,21 @@ function App() {
         <Link to='/' style={{ padding: 5 }}>
           Home
         </Link>
-        <Link to='/barchart' style={{ padding: 5 }}>
+        <Link to='/chart' style={{ padding: 5 }}>
           Bar Chart Animation
         </Link>
-        <Link to='/tree' style={{ padding: 5 }}>
+        {/* <Link to='/tree' style={{ padding: 5 }}>
           Collapsible Tree Animation
         </Link>
         <Link to='/sphere' style={{ padding: 5 }}>
           Vertex Sphere
-        </Link>
+        </Link> */}
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/barchart' element={<BarChart />} />
-        <Route path='/tree' element={<Tree />} />
-        <Route path='/sphere' element={<Sphere />} />
+        <Route path='/chart' element={<BarChart />} />
+        {/* <Route path='/tree' element={<Tree />} />
+        <Route path='/sphere' element={<Sphere />} /> */}
       </Routes>
     </Router>
   );
@@ -35,33 +36,6 @@ function Home() {
     <div style={{ padding: 20 }}>
       <h2>Home View</h2>
       <p>Click Nav Menus to display animations</p>
-    </div>
-  );
-}
-
-function BarChart() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>BarChart View</h2>
-      <p>BarChart View paragraph.</p>
-    </div>
-  );
-}
-
-function Tree() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Tree View</h2>
-      <p>Tree View paragraph.</p>
-    </div>
-  );
-}
-
-function Sphere() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Sphere View</h2>
-      <p>Sphere View paragraph.</p>
     </div>
   );
 }
