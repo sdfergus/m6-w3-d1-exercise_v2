9aadafa2f21ba599011b6d7fddb22c237f870d5d
Shandira <shandira.ferguson@gmail.com>
2023-02-17 19:03:54 -0800

    vertex sphere added. WIP

--- animations-app/src/App.js
+++ animations-app/src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 import BarChart from './Chart/BarChart';
 import Tree from './Tree/Tree';
 import data from './Tree/data';
+import Scene from './Sphere/Scene';
+
+import { Canvas } from "@react-vertex/core";
+import { useViewportSize } from "@react-vertex/dom-hooks";
 
 function App() {
 
+  const { width, height } = useViewportSize();
   return (
     <Router>
       <nav style={{ margin: 10 }}>
@@ -19,15 +24,19 @@ function App() {
         <Link to='/tree' style={{ padding: 5 }}>
           Collapsible Tree Animation
         </Link>
-        {/* <Link to='/sphere' style={{ padding: 5 }}>
+        <Link to='/sphere' style={{ padding: 5 }}>
           Vertex Sphere
-        </Link> */}
+        </Link>
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/chart' element={<BarChart />} />
         <Route path='/tree' element={<Tree data={data} width={600} height={500} />} />
-        {/* <Route path='/sphere' element={<Sphere />} /> */}
+        <Route path='/sphere' element={
+          <Canvas width={width} height={height}>
+            <Scene />
+          </Canvas>
+        } />
       </Routes>
     </Router>
   );
